2a4ceab1b8cdda7cf309465e3cdf4cd877d26c53
João Vitor Gil da Silva <joaovitorgil@yahoo.com.br>
2022-04-27 12:54:42 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-spring-gateway-zuul/src/main/java/com/joaogil/hrapigatewayzuul/HrSpringGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.joaogil.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSpringGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrSpringGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ hr-spring-gateway-zuul/src/test/java/com/joaogil/hrapigatewayzuul/HrSpringGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaogil.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSpringGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
